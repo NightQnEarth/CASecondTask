8d265492a3bb383b07f4b920853859576eef3afa
NightQnEarth <Denis1.618@yandex.ru>
2019-03-09 02:54:39 +0500

    Alignment corrected.

--- CASecondTask/Program.cs
+++ CASecondTask/Program.cs
@@ -17,9 +17,7 @@ namespace CASecondTask
         }
         
         public static string ResultGenerate(IEnumerable<Node> cycle) => 
-            cycle == null ?
-                "A" : 
-                string.Join(Environment.NewLine, "N", string.Join(Environment.NewLine, cycle));
+            cycle == null ? "A" : string.Join(Environment.NewLine, "N", string.Join(Environment.NewLine, cycle));
 
         public static Graph GetInputData(Func<string> lineReader)
         {   
