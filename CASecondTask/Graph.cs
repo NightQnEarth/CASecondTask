9481889d43d0a117c38aa95a11d74c8303b7fbfd
NightQnEarth <Denis1.618@yandex.ru>
2019-03-24 13:28:58 +0500

    Renamed private field from old convention to new.

--- CASecondTask/Graph.cs
+++ CASecondTask/Graph.cs
@@ -5,15 +5,15 @@ namespace CASecondTask
 {
     public class Graph
     {
-        private readonly Node[] _nodes;
+        private readonly Node[] nodes;
 
         public Graph(int nodesCount) =>
-            _nodes = Enumerable.Range(1, nodesCount)
-                               .Select(nodeNumber => new Node(nodeNumber))
-                               .ToArray();
+            nodes = Enumerable.Range(1, nodesCount)
+                              .Select(nodeNumber => new Node(nodeNumber))
+                              .ToArray();
 
-        public IEnumerable<Node> Nodes => _nodes.Select(node => node);
+        public IEnumerable<Node> Nodes => nodes.Select(node => node);
 
-        public Node this[int nodeNumber] => _nodes[nodeNumber];
+        public Node this[int nodeNumber] => nodes[nodeNumber];
     }
 }
